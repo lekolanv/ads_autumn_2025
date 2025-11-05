c856e83f19ae7a2cfcc0195032877b9b718a5a34
lekol <85606378+lekolanv@users.noreply.github.com>
2025-11-05 22:37:44 +0300

    Update D.cpp

--- contest1/D.cpp
+++ contest1/D.cpp
@@ -2,70 +2,73 @@
 #include <iostream>
 #include <vector>
 
-int Findr(uint64_t d) {
-  int c;
-  c = 0;
-  const int cW = 10;
-  while (d != 0) {
-    d /= cW;
-    c++;
-  }
-  return c;
-}
+int const bC = 256;
 
-uint64_t FindMax(const std::vector<uint64_t>& a) {
-  int n = a.size();
-  uint64_t max = a[0];
-  for (int i = 1; i < n; ++i) {
-    if (a[i] > max) {
-      max = a[i];
+int BytesCount(uint64_t d) {
+    if (d == 0) {
+      return 1;
     }
-  }
-  return max;
+    int c = 0;
+    while (d > 0) {
+        c++;
+        d >>= 8;
+    }
+    return c;
 }
 
-void StableDigitSort(std::vector<uint64_t>& a, uint64_t divisor) {
-  int n = a.size();
-  const int cW = 10;
-  std::vector<uint64_t> res(n);
-  std::vector<int> count(cW, 0);
-  for (int i = 0; i < n; i++) {
-    int digit = (a[i] / divisor) % cW;
-    count[digit]++;
-  }
-  for (int i = 1; i < cW; ++i) {
-    count[i] += count[i - 1];
-  }
-  for (int i = n - 1; i >= 0; --i) {
-    int digit = (a[i] / divisor) % cW;
-    res[count[digit] - 1] = a[i];
-    count[digit]--;
-  }
-  for (int i = 0; i < n; ++i) {
-    a[i] = res[i];
-  }
+uint64_t getMax(const std::vector<uint64_t>& a) {
+    uint64_t max = a[0];
+    for (size_t i = 1; i < a.size(); ++i) {
+      if (a[i] > max) {
+        max = a[i];
+      }
+    }
+    return max;
+}
+
+void ByteSort(std::vector<uint64_t>& a, int b_idx) {
+    int n = static_cast<int>(a.size());
+    std::vector<uint64_t> temp(n);
+    std::vector<int> c(bC, 0);
+    for (int i = 0; i < n; ++i) {
+        int b_val = (a[i] >> (b_idx * 8)) & 0xFF;
+        c[b_val]++;
+    }
+    for (int i = 1; i < bC; ++i) {
+        c[i] += c[i - 1];
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        int b_val = (a[i] >> (b_idx * 8)) & 0xFF;
+        temp[c[b_val] - 1] = a[i];
+        c[b_val]--;
+    }
+    for (int i = 0; i < n; ++i) {
+        a[i] = temp[i];
+    }
 }
 
 void LSD(std::vector<uint64_t>& a) {
-  const int cW = 10;
-  uint64_t max = FindMax(a);
-  int r = Findr(max);
-  uint64_t divisor = 1;
-  for (int i = 0; i < r; ++i) {
-    StableDigitSort(a, divisor);
-    divisor *= cW;
-  }
+    uint64_t max_val = getMax(a);
+    int num_bytes = BytesCount(max_val);
+    for (int byte = 0; byte < num_bytes; ++byte) {
+        ByteSort(a, byte);
+    }
 }
 
 int main() {
   int n;
   std::cin >> n;
   std::vector<uint64_t> a(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; ++i) {
     std::cin >> a[i];
   }
   LSD(a);
-  for (auto x : a) {
-    std::cout << x << " ";
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cout << a[i];
+    if (i + 1 < a.size()){
+      std::cout << " ";
+    }
+    std::cout << std::endl;
+    return 0;
   }
 }
